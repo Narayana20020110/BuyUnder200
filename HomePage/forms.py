b679c3c772e23178ed02c8a71ab30f090fd54563
kumar-reddy-kunchala <kreddy0597@gmail.com>
2025-02-18 17:09:40 +0530

    staticfiles

--- HomePage/forms.py
+++ HomePage/forms.py
@@ -4,4 +4,4 @@ from .models import Customer
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['name', 'mobile', 'address', 'email', 'quantity']
+        fields = ['name', 'mobile', 'address', 'email', 'size','quantity']
